6d6993e3be7d18fdb8cdc60e16ba1b27f92fff71
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-21 23:13:20 +0530

    footer completed

---
+++ appendProjectOne/src/component/NavBar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import 'bootstrap/dist/css/main.css'
+
+const NavBar = () => {
+  return (
+    <>
+     <div className='container display-flex'>
+       
+        <div className='nav-logo'>
+            LOGO
+        </div>
+        <div className='nav-link'>
+
+            <ul className='list-item'>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+                <li>
+                    <a href="">Service</a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li><li>
+                    <a href=""></a>
+                </li><li>
+                    <a href=""></a>
+                </li><li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li><li>
+                    <a href=""></a>
+                </li>
+
+            </ul>
+
+        </div>
+
+
+
+
+      
+      </div>
+    
+    
+    
+    </>
+  )
+}
+
+export default NavBar
